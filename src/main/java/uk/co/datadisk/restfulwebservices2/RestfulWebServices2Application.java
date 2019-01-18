12bd0f2fa97e962dfb381619a5ebde3b7dd56a39
pvalle <paul.valle@orbian.com>
2019-01-18 08:11:14 +0000

    Initial commit

---
+++ src/main/java/uk/co/datadisk/restfulwebservices2/RestfulWebServices2Application.java
@@ -0,0 +1,14 @@
+package uk.co.datadisk.restfulwebservices2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServices2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServices2Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/uk/co/datadisk/restfulwebservices2/RestfulWebServices2ApplicationTests.java
@@ -0,0 +1,17 @@
+package uk.co.datadisk.restfulwebservices2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebServices2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
