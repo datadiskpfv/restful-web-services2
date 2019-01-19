f70ec7ff5af36cf8caa6aa84a9280a0893f6ee88
datadiskpfv <paul.valle@datadisk.co.uk>
2019-01-19 11:26:58 +0000

    working on internationalization

--- src/main/java/uk/co/datadisk/restfulwebservices2/RestfulWebServices2Application.java
+++ src/main/java/uk/co/datadisk/restfulwebservices2/RestfulWebServices2Application.java
@@ -2,6 +2,13 @@ package uk.co.datadisk.restfulwebservices2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebServices2Application {
@@ -10,5 +17,19 @@ public class RestfulWebServices2Application {
         SpringApplication.run(RestfulWebServices2Application.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    // Now set in application.properties
+//    @Bean
+//    public ResourceBundleMessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("messages");
+//        return messageSource;
+//    }
 }
 
